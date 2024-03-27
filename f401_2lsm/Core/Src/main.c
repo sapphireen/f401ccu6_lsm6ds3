35c92c4d759e7d74df3468403beca56728f7abcd
odmen <odmen@192.168.2.217>
2024-03-27 12:02:14 +0200

    Add functional

--- f401_2lsm/Core/Src/main.c
+++ f401_2lsm/Core/Src/main.c
@@ -18,10 +18,11 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "usb_device.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-//Initial comit
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -31,7 +32,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define LSM6DS3_I2C_ADDRESS 0xD6
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -40,13 +41,24 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+I2C_HandleTypeDef hi2c2;
 
 /* USER CODE BEGIN PV */
-
+int16_t acc1_x, acc1_y, acc1_z, gyro1_x, gyro1_y, gyro1_z, acc2_x, acc2_y, acc2_z, gyro2_x, gyro2_y, gyro2_z;
+char senrData[100];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_I2C2_Init(void);
+void LSM6DS3_Init(void);
+void LSM6DS3_ReadRawData(int16_t* acc1_x, int16_t* acc1_y, int16_t* acc1_z,
+		int16_t* gyro1_x, int16_t* gyro1_y, int16_t* gyro1_z,
+		int16_t* acc2_x, int16_t* acc2_y, int16_t* acc2_z,
+		int16_t* gyro2_x, int16_t* gyro2_y, int16_t* gyro2_z);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -84,17 +96,28 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_I2C2_Init();
+  MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
-
+  LSM6DS3_Init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  LSM6DS3_ReadRawData(&acc1_x, &acc1_y, &acc1_z, &gyro1_x, &gyro1_y, &gyro1_z, &acc2_x, &acc2_y, &acc2_z, &gyro2_x, &gyro2_y, &gyro2_z);
+	  sprintf(senrData,"%d %d %d %d %d %d %d %d %d %d %d %d \r\n",
+			  acc1_x, acc1_y, acc1_z, gyro1_x, gyro1_y, gyro1_z,
+			  acc2_x, acc2_y, acc2_z, gyro2_x, gyro2_y, gyro2_z);
+	  CDC_Transmit_FS(senrData,strlen(senrData));
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+	  HAL_Delay(100);
   }
   /* USER CODE END 3 */
 }
@@ -116,10 +139,16 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 15;
+  RCC_OscInitStruct.PLL.PLLN = 144;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 5;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -140,8 +169,123 @@ void SystemClock_Config(void)
   }
 }
 
-/* USER CODE BEGIN 4 */
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
 
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 100000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+void LSM6DS3_Init(void)
+{
+	uint8_t init_data[] = {0x10, 0x80};
+	HAL_I2C_Mem_Write(&hi2c1, LSM6DS3_I2C_ADDRESS, 0x10, I2C_MEMADD_SIZE_8BIT, init_data, sizeof(init_data), HAL_MAX_DELAY);
+	HAL_I2C_Mem_Write(&hi2c2, LSM6DS3_I2C_ADDRESS, 0x10, I2C_MEMADD_SIZE_8BIT, init_data, sizeof(init_data), HAL_MAX_DELAY);
+}
+
+void LSM6DS3_ReadRawData(int16_t* acc1_x, int16_t* acc1_y, int16_t* acc1_z,
+		int16_t* gyro1_x, int16_t* gyro1_y, int16_t* gyro1_z,
+		int16_t* acc2_x, int16_t* acc2_y, int16_t* acc2_z,
+		int16_t* gyro2_x, int16_t* gyro2_y, int16_t* gyro2_z)
+{
+	uint8_t buffer1[14];
+	uint8_t buffer2[14];
+	HAL_I2C_Mem_Read(&hi2c1, LSM6DS3_I2C_ADDRESS, 0x22, I2C_MEMADD_SIZE_8BIT, buffer1, sizeof(buffer1), HAL_MAX_DELAY);
+	*acc1_x = (int16_t)(buffer1[1] << 8 | buffer1 [0]);
+	*acc1_y = (int16_t)(buffer1[3] << 8 | buffer1 [2]);
+	*acc1_z = (int16_t)(buffer1[5] << 8 | buffer1 [4]);
+	*gyro1_x = (int16_t)(buffer1[7] << 8 | buffer1 [6]);
+	*gyro1_y = (int16_t)(buffer1[9] << 8 | buffer1 [8]);
+	*gyro1_z = (int16_t)(buffer1[11] << 8 | buffer1 [10]);
+	HAL_I2C_Mem_Read(&hi2c2, LSM6DS3_I2C_ADDRESS, 0x22, I2C_MEMADD_SIZE_8BIT, buffer2, sizeof(buffer2), HAL_MAX_DELAY);
+	*acc2_x = (int16_t)(buffer2[1] << 8 | buffer2 [0]);
+	*acc2_y = (int16_t)(buffer2[3] << 8 | buffer2 [2]);
+	*acc2_z = (int16_t)(buffer2[5] << 8 | buffer2 [4]);
+	*gyro2_x = (int16_t)(buffer2[7] << 8 | buffer2 [6]);
+	*gyro2_y = (int16_t)(buffer2[9] << 8 | buffer2 [8]);
+	*gyro2_z = (int16_t)(buffer2[11] << 8 | buffer2 [10]);
+}
 /* USER CODE END 4 */
 
 /**
